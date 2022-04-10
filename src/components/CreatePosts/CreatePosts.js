23c0fafcd9feb206d6e33edabaddc14dc3159cba
Mark <computerpc@marks-MacBook-Pro.local>
2022-04-09 20:59:25 -0400

    created posts

--- src/components/CreatePosts/CreatePosts.js
+++ src/components/CreatePosts/CreatePosts.js
@@ -1,25 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../Navbar/Navbar";
 import "./CreatePosts.css";
-
+import { Editor } from "react-draft-wysiwyg";
+import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 const CreatePosts = () => {
   return (
     <div>
       <Navbar />
       <div className="create-posts-main">
         <div className="create-posts-div">
-            <div className="create-posts-div-title"></div>
+          <div className="create-posts-div-title">
+            <h1>Question For Developers</h1>
+          </div>
           <input
             type="text"
             className="create-posts-title"
             placeholder="Title"
           />
-          <div className="create-posts-options"></div>
-          <input type="text" className="create-posts-body" />
+            <button className="create-posts-options">
+              <h1>Submit</h1>
+            </button>
+          <div className="create-posts-body">
+          <Editor
+            toolbarClassName="toolbarClassName"
+            wrapperClassName="wrapperClassName"
+            editorClassName="editorClassName"
+          />
+          </div>
         </div>
-        {/* <input type="text" className="create-posts-title" />
-            <input type="text" className="create-posts-body" /> 
-          <input type="submit" value="Submit" />*/}
       </div>
     </div>
   );
